13f714b30c034c57b659492714dc4ca0c1108d84
LaiElaineTing <156033127+areyoumymummy09@users.noreply.github.com>
2024-10-11 23:00:13 -0400

    Update main.js
    Debugged the fix scrolling problem

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 console.log("Javascript Linked!");
 
-//smooth scroll
-
-// Initialize Lenis
-const lenis = new Lenis();
-
-// Listen for the scroll event and log the event data
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
-
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
 //Scroll for sequence
 
 let tl = gsap.timeline({
